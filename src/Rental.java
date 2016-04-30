99e4b9389032b23d86f7a601b80d5859c68a8ca2
ThunderSL94 <SL-Thunder@gmx.de>
2016-04-30 16:01:30 +0200

    extract method to rental

--- src/Rental.java
+++ src/Rental.java
@@ -6,6 +6,13 @@ class Rental {
         daysRented = newdaysRented;
     }
 
+    public int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            return 2;
+        else
+            return 1;
+    }
+
     public double getCharge(){
         double result = 0;
         switch (getMovie().getPriceCode()) {
